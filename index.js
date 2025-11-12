2ff6de39c2013574f596dc183095bfb1654793d6
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-12 15:42:41 +0600

    conenct mongodb and create properties api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 // middleware
@@ -10,6 +12,56 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Nestora server is running');
 })
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@first-db.5h5o2p2.mongodb.net/?appName=first-db`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run(){
+    try{
+        await client.connect();
+
+        const database = client.db('nestora_db');
+        const usersCollections = database.collection('users');
+        const propertiesCollections = database.collection('properties');
+
+        // user related api
+        app.post('/users', async(req, res)=> {
+            const user = req.body;
+            const result = await usersCollections.insertOne(user);
+            res.send(result);
+        })
+
+        // property related api
+        app.get('/properties', async(req, res)=> {
+            const cursor = propertiesCollections.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/properties', async(req, res)=> {
+            const data = req.body;
+            const result = await propertiesCollections.insertOne(data);
+            res.send(result)
+        })
+
+        await client.db('admin').command({ping: 1});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port, ()=> {
     console.log(`Nestora app listening on port ${port}`);
