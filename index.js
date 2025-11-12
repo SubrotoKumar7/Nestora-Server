de0bef05007bdf97da27fb975901cc932c8415ff
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-13 01:44:11 +0600

    single property api create

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run(){
             res.send(result);
         })
 
+        app.get('/properties/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await propertiesCollections.findOne(query);
+            res.send(result);
+        })
+
         app.post('/properties', async(req, res)=> {
             const data = req.body;
             const result = await propertiesCollections.insertOne(data);
